97a3ebe5b4efe2cddc5fcd64a3d69c5d03d3f5df
Sumit <126980669+Pysum@users.noreply.github.com>
2023-04-10 21:40:50 +0530

    Update GUIversion.py

--- GUIversion.py
+++ GUIversion.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import openai
 
-openai.api_key = "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"
+openai.api_key = "YOUR API KEY"
 model_engine = "text-davinci-003"
 chatbot_prompt = """
 As an advanced chatbot, your primary goal is to assist users to the best of your ability. This may involve answering questions, providing helpful information, or completing tasks based on user input. In order to effectively assist users, it is important to be detailed and thorough in your responses. Use examples and evidence to support your points and justify your recommendations or solutions.
